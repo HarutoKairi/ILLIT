47d807f3441d998e0fd43965330227e316dbcc96
Haruto Kairi <chieunguyentat@gmail.com>
2025-08-13 01:14:04 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -316,13 +316,7 @@ function clicked(element){
 
 
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/js/sw.js')
-      .then(reg => console.log('Service Worker registered', reg))
-      .catch(err => console.log('Service Worker registration failed', err));
-  });
-} 
+
 
 
 
