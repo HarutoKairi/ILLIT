52c080fb50c257869664710af3d559c0f651dba5
Haruto Kairi <chieunguyentat@gmail.com>
2025-08-13 02:52:16 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,6 +14,15 @@ closemoreMusic = musicList.querySelector("#close");
 
 
 
+// Modernizr-style feature detection
+if ('CSS' in window && 'supports' in window.CSS) {
+  document.documentElement.classList.add('css-vars');
+}
+
+if ('ontouchstart' in window) {
+  document.documentElement.classList.add('touch-device');
+}
+
 
 
 
